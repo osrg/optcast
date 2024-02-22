aafdb9532165cc6f7e534ac1cf94a30b5fa3d52e
Wataru Ishida <wataru.ishid@gmail.com>
2024-02-22 04:30:28 +0000

    feat(client): support multiple channels, send multiple requests at once
    Signed-off-by: Wataru Ishida <wataru.ishid@gmail.com>

--- reduction_server/src/main.rs
+++ reduction_server/src/main.rs
@@ -24,10 +24,24 @@ use log::{info, trace, warn};
 use num_traits::FromPrimitive;
 
 mod nccl_net;
-use nccl_net::{Comm, Request};
+use nccl_net::{Comm, MemoryHandle, Request};
 
 mod partitioned_vec;
 use partitioned_vec::PartitionedVec;
+
+fn transpose<T>(v: Vec<Vec<T>>) -> Vec<Vec<T>> {
+    assert!(!v.is_empty());
+    let len = v[0].len();
+    let mut iters: Vec<_> = v.into_iter().map(|n| n.into_iter()).collect();
+    (0..len)
+        .map(|_| {
+            iters
+                .iter_mut()
+                .map(|n| n.next().unwrap())
+                .collect::<Vec<T>>()
+        })
+        .collect()
+}
 
 #[derive(Debug, Clone, Copy, PartialEq, Eq, ValueEnum)]
 enum DataType {
@@ -66,6 +80,9 @@ struct Args {
 
     #[arg(long, default_value = "0")] // 0: = nrank
     send_threads: usize,
+
+    #[arg(long, default_value = "1")]
+    nchannel: usize,
 
     #[arg(long, default_value = "1")]
     nrank: usize,
@@ -724,95 +741,212 @@ trait Float: num_traits::Float + FromPrimitive + Default + Sync + Send + std::fm
 impl Float for f32 {}
 impl Float for f16 {}
 
-fn do_client<T: Float>(args: Args, scomm: Comm, rcomm: Comm) {
+fn do_client<T: Float>(args: &Args, comms: Vec<(Comm, Comm)>) {
     let size = args.count * std::mem::size_of::<T>();
     let initial: T = T::from_f32(2.0).unwrap();
-    let mut sbuf =
-        AlignedBox::<[T]>::slice_from_value(alignment(size), args.count, initial).unwrap();
-    let mut rbuf = AlignedBox::<[T]>::slice_from_default(alignment(size), args.count).unwrap();
-
-    let s_mhandle = nccl_net::reg_mr(&scomm, &mut sbuf).unwrap();
-    let r_mhandle = nccl_net::reg_mr(&rcomm, &mut rbuf).unwrap();
 
     let tag = 0x69;
 
-    for i in 0..args.try_count {
-        let mut srequest: Option<Request> = None;
-        let mut rrequest: Option<Request> = None;
+    let mut reqs: [(
+        Option<Vec<(Option<Request>, Option<Request>)>>,
+        PartitionedVec<'_, T>,
+        PartitionedVec<'_, T>,
+        Vec<(MemoryHandle<'_>, MemoryHandle<'_>)>,
+    ); 4] = core::array::from_fn(|_| {
+        let sbuf = PartitionedVec::<T>::from_value(
+            alignment(size),
+            args.count * comms.len(),
+            comms.len(),
+            initial,
+        )
+        .unwrap();
+        let rbuf = PartitionedVec::<T>::new(alignment(size), args.count * comms.len(), comms.len())
+            .unwrap();
 
-        loop {
-            if srequest.is_none() {
-                srequest = nccl_net::isend(&scomm, &s_mhandle, &sbuf, tag).unwrap();
-            }
-            if rrequest.is_none() {
-                rrequest = nccl_net::irecv(&rcomm, &r_mhandle, &mut rbuf, tag).unwrap();
-            }
-            if srequest.is_some() && rrequest.is_some() {
-                break;
-            }
-        }
+        let mhs = comms
+            .iter()
+            .enumerate()
+            .map(|(i, (scomm, rcomm))| {
+                let s_mhandle =
+                    nccl_net::reg_mr(scomm, &mut sbuf.parts[i].lock().unwrap()).unwrap();
+                let r_mhandle =
+                    nccl_net::reg_mr(rcomm, &mut rbuf.parts[i].lock().unwrap()).unwrap();
+                (s_mhandle, r_mhandle)
+            })
+            .collect::<Vec<_>>();
 
-        let mut recv_done = false;
-        let mut send_done = false;
+        (None, sbuf, rbuf, mhs)
+    });
 
-        loop {
-            if !send_done {
-                (send_done, _) = nccl_net::test(srequest.as_ref().unwrap()).unwrap();
-            }
-            if !recv_done {
-                (recv_done, _) = nccl_net::test(rrequest.as_ref().unwrap()).unwrap();
-            }
-            if send_done && recv_done {
-                break;
-            }
-        }
+    let mut finished = 0;
+    let mut reqed = 0;
 
-        let check = rbuf.iter().all(|v| {
-            (*v - initial * T::from_f32(args.nrank as f32).unwrap()).abs()
-                < T::from_f32(1e-6).unwrap()
-        });
-
-        info!("check({}): {}", i, check);
-        if !check {
-            info!("{:?}", rbuf.as_ref());
-        }
-    }
-}
-
-fn client(args: Args) {
-    let mut stream = TcpStream::connect(format!("{}:{}", args.address, args.port))
-        .expect("Could not connect to server");
-    let mut buffer = [0u8; 4];
-    stream.read(buffer.as_mut()).unwrap();
-    let size = u32::from_le_bytes(buffer);
-    let mut handle = vec![0u8; size as usize];
-    stream.read(handle.as_mut()).unwrap();
-    info!("received handle: {:?}", handle);
-
-    let (lcomm, lhandle) = nccl_net::listen().unwrap();
-
-    // send size of handle
-    let size = lhandle.len() as u32;
-    stream.write_all(&size.to_le_bytes()).unwrap();
-    // send handle
-    stream.write_all(&lhandle).unwrap();
-
-    let mut scomm: Option<Comm> = None;
-    let mut rcomm: Option<Comm> = None;
+    // start timer
+    let start = std::time::Instant::now();
 
     loop {
-        if scomm.is_none() {
-            scomm = nccl_net::connect(handle.as_slice()).unwrap();
+        for (req, sbuf, rbuf, mhs) in reqs.iter_mut() {
+            if req.is_none() && reqed < args.try_count {
+                *req = Some(
+                    comms
+                        .iter()
+                        .enumerate()
+                        .map(|(j, (scomm, rcomm))| {
+                            let (s_mhandle, r_mhandle) = &mhs[j];
+                            let mut srequest: Option<Request> = None;
+                            let mut rrequest: Option<Request> = None;
+
+                            loop {
+                                if srequest.is_none() {
+                                    srequest = nccl_net::isend(
+                                        scomm,
+                                        s_mhandle,
+                                        &sbuf.parts[j].lock().unwrap(),
+                                        tag,
+                                    )
+                                    .unwrap();
+                                }
+                                if rrequest.is_none() {
+                                    rrequest = nccl_net::irecv(
+                                        rcomm,
+                                        r_mhandle,
+                                        &mut rbuf.parts[j].lock().unwrap(),
+                                        tag,
+                                    )
+                                    .unwrap();
+                                }
+                                if srequest.is_some() && rrequest.is_some() {
+                                    break;
+                                }
+                            }
+                            (srequest, rrequest)
+                        })
+                        .collect::<Vec<_>>(),
+                );
+                reqed += 1;
+            }
+
+            if req.is_some() {
+                let mut all_done = true;
+                for (j, (srequest, rrequest)) in req.as_mut().unwrap().iter_mut().enumerate() {
+                    if srequest.is_some() {
+                        let (send_done, _) = nccl_net::test(&srequest.as_ref().unwrap()).unwrap();
+                        if send_done {
+                            *srequest = None;
+                        }
+                    }
+                    if rrequest.is_some() {
+                        let (recv_done, _) = nccl_net::test(&rrequest.as_ref().unwrap()).unwrap();
+                        if recv_done {
+                            *rrequest = None;
+                        }
+                    }
+                    if srequest.is_some() || rrequest.is_some() {
+                        all_done = false
+                    }
+                }
+                if all_done {
+                    finished += 1;
+                    *req = None;
+                }
+            }
         }
-        if rcomm.is_none() {
-            rcomm = nccl_net::accept(&lcomm).unwrap();
-        }
-        if scomm.is_some() && rcomm.is_some() {
+
+        if finished == args.try_count {
             break;
         }
     }
-    let scomm = scomm.unwrap();
-    let rcomm = rcomm.unwrap();
+
+    // stop timer
+    let elapsed = start.elapsed();
+    print_stat(
+        args.count * std::mem::size_of::<T>() * comms.len(),
+        elapsed.as_micros() / args.try_count as u128,
+    );
+}
+
+fn print_stat(size: usize, latency: u128) {
+    let size = size as f64; // B
+    let latency = latency as f64 / 1000.0 / 1000.0; // s
+    let bandwidth = (size * 8.0) / latency; // bps
+    let bandwidth = bandwidth / 1024.0 / 1024.0 / 1024.0; // Gbps
+    info!(
+        "size: {:.2}MB, bandwidth: {:.2}Gbps",
+        size / 1024.0 / 1024.0,
+        bandwidth
+    );
+}
+
+fn client(args: Args) {
+    let (streams, comms): (Vec<TcpStream>, Vec<Vec<(Comm, Comm)>>) = args
+        .address
+        .split(',')
+        .map(|addr| {
+            info!("connecting to {}", addr);
+            let mut stream = TcpStream::connect(addr).expect("Could not connect to server");
+
+            let comms = (0..args.nchannel)
+                .map(|_| {
+                    let mut buffer = [0u8; 4];
+                    stream.read(buffer.as_mut()).unwrap();
+                    let size = u32::from_le_bytes(buffer);
+                    let mut handle = vec![0u8; size as usize];
+                    stream.read(handle.as_mut()).unwrap();
+                    info!("received handle: {:?}", handle);
+
+                    let (lcomm, lhandle) = nccl_net::listen().unwrap();
+
+                    // send size of handle
+                    let size = lhandle.len() as u32;
+                    stream.write_all(&size.to_le_bytes()).unwrap();
+                    // send handle
+                    stream.write_all(&lhandle).unwrap();
+
+                    let mut scomm: Option<Comm> = None;
+                    let mut rcomm: Option<Comm> = None;
+
+                    loop {
+                        if scomm.is_none() {
+                            scomm = nccl_net::connect(handle.as_slice()).unwrap();
+                        }
+                        if rcomm.is_none() {
+                            rcomm = nccl_net::accept(&lcomm).unwrap();
+                        }
+                        if scomm.is_some() && rcomm.is_some() {
+                            break;
+                        }
+                    }
+
+                    let scomm = scomm.unwrap();
+                    let rcomm = rcomm.unwrap();
+                    (scomm, rcomm)
+                })
+                .collect::<Vec<_>>();
+            (stream, comms) // return stream to keep the socket open until we finish
+        })
+        .unzip();
+
+    let comms = transpose(comms);
+
+    info!("client connected");
+
+    let args = Arc::new(args);
+
+    let hs = comms.into_iter()
+        .map(|comm| {
+            let args = Arc::clone(&args);
+            std::thread::spawn(move || {
+                if args.data_type == DataType::F32 {
+                    do_client::<f32>(args.as_ref(), comm);
+                } else if args.data_type == DataType::F16 {
+                    do_client::<f16>(args.as_ref(), comm);
+                }
+            })
+        })
+        .collect::<Vec<_>>();
+    hs.into_iter().for_each(|h| h.join().unwrap());
+    drop(streams);
+}
 
     info!("client connected");
 

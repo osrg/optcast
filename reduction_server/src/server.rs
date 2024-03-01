28bd5bb40947587307426045bf5ec27de3b44a6a
Wataru Ishida <wataru.ishid@gmail.com>
2024-03-01 05:08:44 +0000

    fix(ring): use SIMD for reduce
    Signed-off-by: Wataru Ishida <wataru.ishid@gmail.com>

--- reduction_server/src/server.rs
+++ reduction_server/src/server.rs
@@ -11,10 +11,8 @@ use std::net::TcpListener;
 use std::sync::atomic::AtomicUsize;
 use std::sync::Arc;
 
-use aligned_box::AlignedBox;
-use half::f16;
-use half::slice::HalfFloatSliceExt;
 use log::{info, trace, warn};
+use half::f16;
 
 use crate::utils::*;
 
@@ -71,87 +69,6 @@ fn handle_connection(
     info!("handle_connection: exiting ret {:?}", ret);
 
     rank.fetch_sub(1, std::sync::atomic::Ordering::Relaxed);
-}
-
-struct WorkingMemory {
-    recv_bufs: Vec<AlignedBox<[f32]>>,
-    send_buf: AlignedBox<[f32]>,
-}
-
-impl WorkingMemory {
-    fn new(count: usize, num_recv: usize) -> Self {
-        let recv_bufs = (0..num_recv)
-            .map(|_| AlignedBox::<[f32]>::slice_from_default(alignment(count), count).unwrap())
-            .collect::<Vec<_>>();
-        let send_buf = AlignedBox::<[f32]>::slice_from_default(alignment(count), count).unwrap();
-        Self {
-            recv_bufs,
-            send_buf,
-        }
-    }
-}
-
-trait Reduce<T> {
-    fn reduce(
-        &mut self,
-        recv_bufs: &Vec<&[T]>,
-        work_mem: Option<&mut WorkingMemory>,
-    ) -> Result<(), ()>;
-}
-
-impl<T: Float> Reduce<T> for [T] {
-    default fn reduce(&mut self, _: &Vec<&[T]>, _: Option<&mut WorkingMemory>) -> Result<(), ()> {
-        Err(())
-    }
-}
-
-impl Reduce<f16> for [f16] {
-    fn reduce(
-        &mut self,
-        recv_bufs: &Vec<&[f16]>,
-        work_mem: Option<&mut WorkingMemory>,
-    ) -> Result<(), ()> {
-        let work_mem = work_mem.unwrap();
-        for (i, recv) in recv_bufs.iter().enumerate() {
-            recv.convert_to_f32_slice(&mut work_mem.recv_bufs[i].as_mut());
-        }
-        work_mem.send_buf.reduce(
-            &work_mem
-                .recv_bufs
-                .iter()
-                .map(|v| {
-                    let slice_ref: &[f32] = &**v;
-                    slice_ref
-                })
-                .collect(),
-            None,
-        )?;
-        self.as_mut()
-            .convert_from_f32_slice(&work_mem.send_buf.as_ref());
-        Ok(())
-    }
-}
-
-// impl<T: Float + std::simd::SimdElement> Reduce<T> for AlignedBox<[T]> can't compile
-// error: cannot specialize on trait `SimdElement`
-// --> src/main.rs:139:17
-// |
-// 139 | impl<T: Float + std::simd::SimdElement> Reduce<T> for AlignedBox<[T]> {
-impl Reduce<f32> for [f32] {
-    fn reduce(&mut self, recv_bufs: &Vec<&[f32]>, _: Option<&mut WorkingMemory>) -> Result<(), ()> {
-        let (_, send, _) = self.as_simd_mut::<4>();
-        for (i, recv) in recv_bufs.iter().enumerate() {
-            let (_, recv, _) = recv.as_ref().as_simd::<4>();
-            if i == 0 {
-                send.copy_from_slice(&recv.as_ref());
-            } else {
-                for j in 0..send.len() {
-                    send[j] += recv[j];
-                }
-            }
-        }
-        Ok(())
-    }
 }
 
 fn reduce_loop<T: Float>(
--- reduction_server/src/utils.rs
+++ reduction_server/src/utils.rs
@@ -6,9 +6,11 @@
 
 use std::fmt::Debug;
 
+use aligned_box::AlignedBox;
 use clap::{Parser, ValueEnum};
 use half::f16;
-use log::info;
+use half::slice::HalfFloatSliceExt;
+use log::trace;
 use num_traits::FromPrimitive;
 
 pub(crate) fn transpose<T>(v: Vec<Vec<T>>) -> Vec<Vec<T>> {
@@ -100,9 +102,119 @@ pub(crate) fn print_stat(size: usize, latency: u128) {
     let latency = latency as f64 / 1000.0 / 1000.0; // s
     let bandwidth = (size * 8.0) / latency; // bps
     let bandwidth = bandwidth / 1024.0 / 1024.0 / 1024.0; // Gbps
-    info!(
+    trace!(
         "size: {:.2}MB, bandwidth: {:.2}Gbps",
         size / 1024.0 / 1024.0,
         bandwidth
     );
+}
+
+pub(crate) struct WorkingMemory {
+    recv_bufs: Vec<AlignedBox<[f32]>>,
+    send_buf: AlignedBox<[f32]>,
+}
+
+impl WorkingMemory {
+    pub(crate) fn new(count: usize, num_recv: usize) -> Self {
+        let recv_bufs = (0..num_recv)
+            .map(|_| AlignedBox::<[f32]>::slice_from_default(alignment(count), count).unwrap())
+            .collect::<Vec<_>>();
+        let send_buf = AlignedBox::<[f32]>::slice_from_default(alignment(count), count).unwrap();
+        Self {
+            recv_bufs,
+            send_buf,
+        }
+    }
+}
+
+pub(crate) trait Reduce<T> {
+    fn reduce(
+        &mut self,
+        recv_bufs: &Vec<&[T]>,
+        work_mem: Option<&mut WorkingMemory>,
+    ) -> Result<(), ()>;
+    fn reduce_one(&mut self, recv_buf: &[T], _: Option<&mut WorkingMemory>) -> Result<(), ()>;
+}
+
+impl<T: Float> Reduce<T> for [T] {
+    default fn reduce(&mut self, _: &Vec<&[T]>, _: Option<&mut WorkingMemory>) -> Result<(), ()> {
+        Err(())
+    }
+
+    default fn reduce_one(&mut self, _: &[T], _: Option<&mut WorkingMemory>) -> Result<(), ()> {
+        Err(())
+    }
+}
+
+impl Reduce<f16> for [f16] {
+    fn reduce(
+        &mut self,
+        recv_bufs: &Vec<&[f16]>,
+        work_mem: Option<&mut WorkingMemory>,
+    ) -> Result<(), ()> {
+        let work_mem = work_mem.unwrap();
+        for (i, recv) in recv_bufs.iter().enumerate() {
+            recv.convert_to_f32_slice(&mut work_mem.recv_bufs[i].as_mut());
+        }
+        work_mem.send_buf.reduce(
+            &work_mem
+                .recv_bufs
+                .iter()
+                .map(|v| {
+                    let slice_ref: &[f32] = &**v;
+                    slice_ref
+                })
+                .collect(),
+            None,
+        )?;
+        self.as_mut()
+            .convert_from_f32_slice(&work_mem.send_buf.as_ref());
+        Ok(())
+    }
+
+    fn reduce_one(
+        &mut self,
+        recv_buf: &[f16],
+        work_mem: Option<&mut WorkingMemory>,
+    ) -> Result<(), ()> {
+        let work_mem = work_mem.unwrap();
+        recv_buf.convert_to_f32_slice(&mut work_mem.recv_bufs[0].as_mut());
+        work_mem
+            .send_buf
+            .reduce_one(&work_mem.recv_bufs[0].as_ref(), None)?;
+        self.as_mut()
+            .convert_from_f32_slice(&work_mem.send_buf.as_ref());
+        Ok(())
+    }
+}
+
+// impl<T: Float + std::simd::SimdElement> Reduce<T> for AlignedBox<[T]> can't compile
+// error: cannot specialize on trait `SimdElement`
+// --> src/main.rs:139:17
+// |
+// 139 | impl<T: Float + std::simd::SimdElement> Reduce<T> for AlignedBox<[T]> {
+impl Reduce<f32> for [f32] {
+    fn reduce(&mut self, recv_bufs: &Vec<&[f32]>, _: Option<&mut WorkingMemory>) -> Result<(), ()> {
+        let (_, send, _) = self.as_simd_mut::<4>();
+        for (i, recv) in recv_bufs.iter().enumerate() {
+            let (_, recv, _) = recv.as_ref().as_simd::<4>();
+            if i == 0 {
+                send.copy_from_slice(&recv.as_ref());
+            } else {
+                for j in 0..send.len() {
+                    send[j] += recv[j];
+                }
+            }
+        }
+        Ok(())
+    }
+
+    fn reduce_one(&mut self, recv_buf: &[f32], _: Option<&mut WorkingMemory>) -> Result<(), ()> {
+        let (_, send, _) = self.as_simd_mut::<4>();
+        let (_, recv, _) = recv_buf.as_ref().as_simd::<4>();
+        for j in 0..send.len() {
+            send[j] += recv[j];
+        }
+        Ok(())
+    }
 }
